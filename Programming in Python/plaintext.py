bd39ed388a0f9a1eaa83e6d764c33c7c301c4a66
eschaton <innocent.renanw@gmail.com>
2022-05-26 13:46:03 +0200

    debugging matplotlib ass3

--- Programming in Python/plaintext.py
+++ Programming in Python/plaintext.py
@@ -1,10 +1,33 @@
-# fix the following code so it runs
-#1
-if True:
-    print("fix me")
-#2
-for i in range(3):
-    print(i)
-#3
-x=[1,2,3,4]
-print(x[3])
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+proteinNames = []
+dielectricConstants = []
+polarizability = []
+volume = []
+
+
+
+#split each line where commas appear, then add discrete parts into relevant lists
+#in separate function because it was easier to debug
+def splitLine(l):
+    temp = l.split(",")
+    proteinNames.append(temp[0])
+    dielectricConstants.append(temp[1])
+    polarizability.append(temp[2])
+    volume.append(temp[3])
+
+
+
+#iterate over entire file line by line
+with open('epsilon.csv') as text:
+    for index, line in enumerate(text):
+        splitLine(line)
+
+
+plt.plot(polarizability, volume, '-', label='Polarizability against Volume')
+#plt.legend(loc='best')
+
+plt.show()
+
